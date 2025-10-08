ea09b79b5118e01baca6d97b2470b358f3b3de10
tokimanana <tokimananasarobidy@gmail.com>
2025-10-08 12:22:29 +0300

    feat: connecting app with firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore({
+      router: routerReducer
+    }),
+    provideEffects([]),
+    provideStoreDevtools(),
+    provideRouterStore(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAm-HNi8kp4o3Vn_XQLxL5A_tfcfPwZXeA',
+    authDomain: 'jira-clone-656d0.firebaseapp.com',
+    projectId: 'jira-clone-656d0',
+    storageBucket: 'jira-clone-656d0.firebasestorage.app',
+    messagingSenderId: '301452877481',
+    appId: '1:301452877481:web:3ba60a9bbcbfd69ee613b8',
+    measurementId: 'G-KX0J1MXJ8Q',
+  },
+};
