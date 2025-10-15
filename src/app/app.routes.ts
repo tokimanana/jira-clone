ed10f390b671f3eae6944bed371b268b0a777277
tokimanana <tokimananasarobidy@gmail.com>
2025-10-15 20:05:52 +0300

    feat(auth): routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register.component').then((m) => m.RegisterComponent),
+  },
+  // {
+  //   path: 'board',
+  //   loadComponent: () => import('./kanban/kanban-board/kanban-board.component').then(m => m.KanbanBoardComponent),
+  //   canActivate: [authGuard]
+  // },
+  {
+    path: '',
+    redirectTo: 'board',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'board',
+  }
+];
