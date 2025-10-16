bdb44bf4762c7fbda30cb6f6c8f6b6329e7d6194
tokimanana <tokimananasarobidy@gmail.com>
2025-10-16 23:06:12 +0300

    feat(auth): login component

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+import { selectAuthState } from '../../store/auth/auth.selector';
+import { AuthActions } from '../../store/auth/auth.action';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  private store = inject(Store);
+
+  //form
+  email ='';
+  password = '';
+
+  //error && isLoading
+  isLoading$ : Observable<boolean> = this.store.select(selectAuthState).pipe(
+    map(loadingState => loadingState.isLoading)
+  )
+
+  error$ : Observable<string | null> = this.store.select(selectAuthState).pipe(
+    map(errorMessage => errorMessage.error)
+  )
+
+  onSubmit() {
+    this.store.dispatch(AuthActions.login({
+        email: this.email,
+        password: this.password
+      }))
+  }
+
+}
