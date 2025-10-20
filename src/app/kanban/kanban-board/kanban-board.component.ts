66a85bcc3bfe2a88108f421a7d4788f9c3e21571
tokimanana <tokimananasarobidy@gmail.com>
2025-10-20 23:43:06 +0300

    feat(kanban): implement Kanban board with drag & drop

--- src/app/kanban/kanban-board/kanban-board.component.ts
+++ src/app/kanban/kanban-board/kanban-board.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Task } from '../../store/tasks/tasks.model';
+import { selectDoneTasks, selectInProgressTasks, selectToDoTasks } from '../../store/tasks/tasks.selector';
+import { TasksAction } from '../../store/tasks/tasks.actions';
+import { CommonModule } from '@angular/common';
+import { TaskCardComponent } from "../task-card/task-card.component";
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban-board',
-  imports: [],
+  imports: [CommonModule, TaskCardComponent, DragDropModule],
   templateUrl: './kanban-board.component.html',
   styleUrl: './kanban-board.component.scss'
 })
-export class KanbanBoardComponent {
+export class KanbanBoardComponent implements OnInit {
 
+  private readonly store = inject(Store);
+
+  toDoTasks$ : Observable<Task[]> = this.store.select(selectToDoTasks);
+  inProgresTasks$ : Observable<Task[]> = this.store.select(selectInProgressTasks);
+  doneTasks$ : Observable<Task[]> = this.store.select(selectDoneTasks);
+
+  ngOnInit(): void {
+      this.store.dispatch(TasksAction.loadTasks())
+  }
 }
