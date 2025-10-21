98c8de7aee1bcd35d36ba316b0bdffcea1872cea
tokimanana <tokimananasarobidy@gmail.com>
2025-10-21 23:59:29 +0300

    feat(tasks): add CRUD actions for tasks

--- src/app/store/tasks/tasks.actions.ts
+++ src/app/store/tasks/tasks.actions.ts
@@ -7,5 +7,22 @@ export const TasksAction = createActionGroup({
     'Load Tasks': emptyProps(),
     'Load Tasks Success': props<{ tasks: Task[] }>(),
     'Load Tasks Failure': props<{ error: string }>(),
+
+    //add
+    'Add Task': props<{ taskData: Omit<Task, 'id'> }>(),
+    'Add Task Success': props<{ task: Task }>(),
+    'Add Task Failure': props<{ error: string }>(),
+
+    //update
+    'Update Task': props<{ task: Partial<Task> & { id: string } }>(),
+    'Update Task Success': props<{ task: Partial<Task> & { id: string } }>(),
+    'Update Task Failure': props<{ error: string }>(),
+
+    //delete
+    'Delete Task': props<{ taskId: string }>(),
+    'Delete Task Success': props<{ taskId: string }>(),
+    'Delete Task Failure': props<{ error: string }>(
+
+    ),
   },
 });
