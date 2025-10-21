1dafed281c3d43c7367fbb9b5c05ed092ff43cc3
tokimanana <tokimananasarobidy@gmail.com>
2025-10-21 23:59:37 +0300

    feat(tasks): implement CRUD operations in TasksServi

--- src/app/store/tasks/tasks.service.ts
+++ src/app/store/tasks/tasks.service.ts
@@ -1,19 +1,41 @@
 import { inject, Injectable } from '@angular/core';
-import { collectionData, Firestore } from '@angular/fire/firestore';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  DocumentReference,
+  Firestore,
+  updateDoc,
+} from '@angular/fire/firestore';
 import { Task } from './tasks.model';
-import { collection } from 'firebase/firestore';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasksService {
   private firestore: Firestore = inject(Firestore);
+  private tasksCollection = collection(this.firestore, 'tasks');
 
   getTasks(): Observable<Task[]> {
-    const tasksCollection = collection(this.firestore, 'tasks');
-    return collectionData(tasksCollection, { idField: 'id' }) as Observable<
-      Task[]
-    >;
+    return collectionData(this.tasksCollection, {
+      idField: 'id',
+    }) as Observable<Task[]>;
+  }
+
+  addTasks(taskData: Omit<Task, 'id'>): Observable<DocumentReference> {
+    return from(addDoc(this.tasksCollection, taskData));
+  }
+
+  updateTask(taskUpdate: Partial<Task> & { id: string }): Observable<void> {
+    const taskDocRef = doc(this.firestore, `tasks/${taskUpdate.id}`);
+    return from(updateDoc(taskDocRef, taskUpdate));
+  }
+
+  deleteTask(taskId: string): Observable<void> {
+    const taskDocRef = doc(this.firestore, `tasks/${taskId}`)
+    return from(deleteDoc(taskDocRef));
   }
 }
