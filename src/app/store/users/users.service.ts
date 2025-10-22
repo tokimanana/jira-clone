79cd6651636f437f78de2abb1667ac1772828ba6
tokimanana <tokimananasarobidy@gmail.com>
2025-10-23 00:53:09 +0300

    feat(users): implement users servise

---
+++ src/app/store/users/users.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { User } from './users.model';
+
+@Injectable({ providedIn: 'root' })
+export class UsersService {
+  private readonly firestore: Firestore = inject(Firestore);
+
+  getUsers(): Observable<User[]> {
+    const usersCollection = collection(this.firestore, 'users');
+    return collectionData(usersCollection) as Observable<User[]>;
+  }
+}
