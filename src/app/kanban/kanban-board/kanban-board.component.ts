3a53bb5b72aed6719b4d66ba48624ec20080feb5
tokimanana <tokimananasarobidy@gmail.com>
2025-10-23 23:59:39 +0300

    feat(kanban): integrate CRUD operations in board

--- src/app/kanban/kanban-board/kanban-board.component.ts
+++ src/app/kanban/kanban-board/kanban-board.component.ts
@@ -6,11 +6,12 @@ import { selectDoneTasks, selectInProgressTasks, selectToDoTasks } from '../../s
 import { TasksAction } from '../../store/tasks/tasks.actions';
 import { CommonModule } from '@angular/common';
 import { TaskCardComponent } from "../task-card/task-card.component";
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
+import { AddEditTaskComponent } from '../add-edit-task/add-edit-task.component';
 
 @Component({
   selector: 'app-kanban-board',
-  imports: [CommonModule, TaskCardComponent, DragDropModule],
+  imports: [CommonModule, TaskCardComponent, DragDropModule, AddEditTaskComponent],
   templateUrl: './kanban-board.component.html',
   styleUrl: './kanban-board.component.scss'
 })
@@ -22,7 +23,48 @@ export class KanbanBoardComponent implements OnInit {
   inProgresTasks$ : Observable<Task[]> = this.store.select(selectInProgressTasks);
   doneTasks$ : Observable<Task[]> = this.store.select(selectDoneTasks);
 
+  isModalOpen = false;
+  editingTask: Task | null = null;
+
   ngOnInit(): void {
-      this.store.dispatch(TasksAction.loadTasks())
+    this.store.dispatch(TasksAction.loadTasks())
+  }
+
+  //openModal
+  openModal(task: Task | null = null): void {
+    this.editingTask = task;
+    this.isModalOpen = true;
+  }
+
+  //closeModal
+  closeModal() : void{
+    this.isModalOpen = false;
+    this.editingTask = null;
+  }
+
+  //deleteTask
+  onDeleteTask(taskId: string): void {
+    if(confirm('Are you sure you want to delete this task?')) {
+      this.store.dispatch(TasksAction.deleteTask({taskId}));
+    }
+  }
+
+  //onDrop
+  onDrop(event: CdkDragDrop<Task[]>): void {
+    if(event.previousContainer === event.container) return;
+    const task = event.previousContainer.data[event.previousIndex];
+    const newStatus = this.getColumnStatus(event.container.id);
+    if(newStatus) {
+      this.store.dispatch(TasksAction.updateTask({
+        task: {id: task.id, status: newStatus}
+      }))
+    }
+  }
+
+  getColumnStatus(containerId: string) {
+    if(containerId.includes('todo')) return 'To Do';
+    if(containerId.includes('inprogress')) return 'In Progress';
+    if(containerId.includes('done')) return 'Done'
+    return null;
   }
 }
