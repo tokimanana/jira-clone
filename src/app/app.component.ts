304341c34d4266d1f4e03a42f406695f68249d14
tokimanana <tokimananasarobidy@gmail.com>
2025-10-29 23:45:00 +0300

    feat(logout): add logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectIsLoggedIn } from './store/auth/auth.selectors';
+import { AuthActions } from './store/auth/auth.action';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'jira-clone';
+
+  private readonly store = inject(Store);
+
+  isLoggedIn$ : Observable<boolean> = this.store.select(selectIsLoggedIn);
+
+  onLogout(): void {
+    this.store.dispatch(AuthActions.logout());
+  }
 }
