3f5e4ea78f57689d0cf5a4ff5c5b1b61ae389bcf
tokimanana <tokimananasarobidy@gmail.com>
2025-11-04 23:52:00 +0300

    feat(toast): add toast component

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-toast',
-  imports: [],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.scss'
 })
 export class ToastComponent {
+  private readonly toastService = inject(ToastService);
 
+  protected readonly toasts$ = this.toastService.getToasts();
+
+  protected remove(id: number): void {
+    this.toastService.remove(id);
+  }
 }
