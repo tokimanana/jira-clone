9599cf5b81e1c1117378483a82798d5f6725d698
tokimanana <tokimananasarobidy@gmail.com>
2025-11-04 23:51:00 +0300

    feat(spinner): add spinner component

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SpinnerService } from '../../services/spinner.service';
 
 @Component({
   selector: 'app-spinner',
-  imports: [],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './spinner.component.html',
   styleUrl: './spinner.component.scss'
 })
 export class SpinnerComponent {
+  private readonly spinnerService = inject(SpinnerService);
 
+  protected readonly loading$ = this.spinnerService.loading$;
 }
