21553d0213dc06c354cfd4b3e5c9ca6139d1c1b7
tokimanana <tokimananasarobidy@gmail.com>
2025-11-04 23:49:00 +0300

    feat(spinner): implement loading service

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpinnerService {
+  private count = 0;
 
-  constructor() { }
+  private readonly isLoading$ = new BehaviorSubject<boolean>(false);
+
+  constructor() {}
+
+  get loading$(): Observable<boolean> {
+    return this.isLoading$.asObservable();
+  }
+
+  show(): void {
+    this.count++;
+    if (this.count === 1) {
+      this.isLoading$.next(true);
+    }
+  }
+
+  hide(): void {
+    this.count = Math.max(0, this.count - 1);
+    if (this.count === 0) {
+      this.isLoading$.next(false);
+    }
+  }
+
+  reset(): void {
+    this.count = 0;
+    this.isLoading$.next(false);
+  }
 }
